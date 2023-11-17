2bc69e99b8e5c04b7cfd534dbc0e3280845b3920
= <=>
2023-11-17 13:52:35 +0100

    start project

---
+++ shopTask/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import ProductsContainer from "./components/ProductsContainer.jsx";
+
+function App() {
+  return (
+    <>
+      <ProductsContainer />
+    </>
+  );
+}
+
+export default App;
